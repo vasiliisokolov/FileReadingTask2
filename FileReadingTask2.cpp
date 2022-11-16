93ee545433b3bfd43e457cc50857710cad339b4f
VasiliiSokolov <vasilysokolov@mail.ru>
2022-11-16 21:10:41 +0300

    open problem

--- FileReadingTask2.cpp
+++ FileReadingTask2.cpp
@@ -1,24 +1,32 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <vector>
+
 
 
 int main()
 {
-    std::vector<char> buffer;
+    char buffer[50];
     std::string adress;
     std::fstream book;
     std::cout << "Enter file adress: \n";
     std::cin >> adress;
+    std::cout << adress << std::endl;
     
-    book.open(adress);
+    book.open(adress, std::ios::binary);
     if (book.is_open())
     {
         std::cout << "File is open!" << std::endl;
+        while (!book.eof())
+        {
+            book.read(buffer, sizeof(buffer-1));
+            buffer[19] = 0;
+            std::cout << buffer << std::endl;
+        }
+        
     }
     else
     {
         std::cerr << "File is not open!" << std::endl;
     }
-
+    book.close();
 }
