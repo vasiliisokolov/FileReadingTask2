33df478d817ade1bacb17207fc1d4f3ab19ace43
VasiliiSokolov <vasilysokolov@mail.ru>
2022-11-16 19:19:30 +0300

    Добавьте файлы проекта.

---
+++ FileReadingTask2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+int main()
+{
+    std::vector<char> buffer;
+    std::string adress;
+    std::fstream book;
+    std::cout << "Enter file adress: \n";
+    std::cin >> adress;
+    
+    book.open(adress);
+    if (book.is_open())
+    {
+        std::cout << "File is open!" << std::endl;
+    }
+    else
+    {
+        std::cerr << "File is not open!" << std::endl;
+    }
+
+}
